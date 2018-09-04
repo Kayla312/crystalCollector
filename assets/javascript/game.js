131b7f620f22e47dcfa3667a0ca27b7926772865
The Singer <kayla312@harbornet.com>
2018-09-03 22:05:00 -0700

    added buttons where the images were beforehand, have a bit of the javascript down- establishing crystal counter and each of the buttons.. still working

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// crystal counter values
+var crystals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12];
+// counts wins and losses
+var wins = 0;
+var losses = 0;
+
+// random number assigned to ruby
+var rubyNumber = function(){
+    crystals[Math.floor(Math.random() * crystals.length)]
+};
+// random number assigned to sapphire
+var sapphireNumber = [];
+// random number assigned to emerald
+var emeraldNumber = [];
+
